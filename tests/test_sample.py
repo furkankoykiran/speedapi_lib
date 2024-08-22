4cd25ffa49b94c6627c171e7c6a54ae8167e8ebe
Furkan Köykıran <furkankoykiran@gmail.com>
2024-08-22 18:41:25 +0300

    Refactor code to use SpeedAPI class for interacting with Speed API

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,10 +1,10 @@
 import unittest
-from speedapi_lib.api_client import SpeedAPIClient
+from speedapi_lib.speedapi import SpeedAPI
 
 class TestSpeedAPIClient(unittest.TestCase):
 
     def test_get_balance_sats(self):
-        client = SpeedAPIClient(secret_key='your_secret_key', base_url='https://api.tryspeed.com')
+        client = SpeedAPI(secret_key='your_secret_key', base_url='https://api.tryspeed.com')
         balance = client.get_balance_sats()
         self.assertIsInstance(balance, float)
 
